bf19f5865f41cb2a85c59d275e724d5ede67e377
Ajit Musalgavkar <ajit.musalgavkar@montageinteractive.com>
2017-06-28 11:00:25 +1200

    added manage author page, reusable form components, transition effects

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -1,7 +1,9 @@
 (function () {
     "use strict";
     var React = require('react');
-
+    var Router = require('react-router');
+    var Link = Router.Link;
+    
     var AuthorList = React.createClass({
         propTypes: {
             authors: React.PropTypes.array.isRequired
@@ -11,7 +13,7 @@
             var createAuthorRow = function (author) {
                 return (
                     <tr key={author.id}>
-                        <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
+                        <td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
                         <td>{author.firstName} {author.lastName}</td>
                     </tr>
                 );
