69b68d86444e3f931bb343f3ec981086b01b18d5
Ajit Musalgavkar <ajit.musalgavkar@montageinteractive.com>
2017-06-27 15:52:02 +1200

    controller views, prop types and mixins

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46313,17 +46313,12 @@ module.exports = require('./lib/React');
 (function () {
     "use strict";
     var React = require('react');
-    var AuthorApi = require('../../api/authorApi');
 
-    var Authors = React.createClass({displayName: "Authors",
-        getInitialState: function () {
-            return {
-                authors: []
-            };
+    var AuthorList = React.createClass({displayName: "AuthorList",
+        propTypes: {
+            authors: React.PropTypes.array.isRequired
         },
-        componentWillMount: function () {
-            this.setState({ authors: AuthorApi.getAllAuthors() });
-        },
+
         render: function () {
             var createAuthorRow = function (author) {
                 return (
@@ -46336,14 +46331,14 @@ module.exports = require('./lib/React');
             };
             return (
                 React.createElement("div", null, 
-                    React.createElement("h1", null, "Authors"), 
+
                     React.createElement("table", {className: "table"}, 
                         React.createElement("thead", null, 
                             React.createElement("th", null, "ID"), 
                             React.createElement("th", null, "Name")
                         ), 
                         React.createElement("tbody", null, 
-                            this.state.authors.map(createAuthorRow, this)
+                            this.props.authors.map(createAuthorRow, this)
                         )
 
                     )
@@ -46352,10 +46347,43 @@ module.exports = require('./lib/React');
         }
     });
 
-    module.exports = Authors;
+    module.exports = AuthorList;
 })();
 
-},{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+},{"react":158}],163:[function(require,module,exports){
+(function () {
+    "use strict";
+    var React = require('react');
+    var AuthorApi = require('../../api/authorApi');
+    var AuthorList = require('./authorList');
+
+    var AuthorPage = React.createClass({displayName: "AuthorPage",
+        getInitialState: function () {
+            return {
+                authors: []
+            };
+        },
+        componentDidMount: function () {
+            if (this.isMounted) {
+                this.setState({ authors: AuthorApi.getAllAuthors() });
+            }
+
+        },
+        render: function () {
+
+            return (
+                React.createElement("div", null, 
+                    React.createElement("h1", null, "Authors"), 
+                    React.createElement(AuthorList, {authors: this.state.authors})
+                )
+            );
+        }
+    });
+
+    module.exports = AuthorPage;
+})();
+
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 (function () {
     "use strict";
     var React = require('react');
@@ -46382,7 +46410,7 @@ module.exports = require('./lib/React');
     module.exports = Header;
 })();
 
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 (function () {
 
     "use strict";
@@ -46402,7 +46430,7 @@ module.exports = require('./lib/React');
 
     module.exports = Home;
 })();
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 $ = jQuery = require('jquery');
 
 var React = require('react');
@@ -46453,4 +46481,4 @@ var Header = require('./components/common/header');
 
 })(window);
 
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
